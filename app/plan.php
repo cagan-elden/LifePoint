3766b588ead8ce7f87b8c64a0a381fb13c54664d
Cagan Eldenk <caganeldenk@gmail.com>
2024-08-11 17:39:59 +0300

    Working on new functions, continue tomorrow.

--- app/plan.php
+++ app/plan.php
@@ -46,29 +46,73 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['date']) && isset($_GET[
         var dashes = [0];
         var dashElementHandler = [5];
 
+        var dateTodObj = new Date();
+        const dateTod = dateTodObj.toLocaleDateString();
+        console.log(dateTod);
+
         $(document).ready(function(){
-            // Need the code below for creating 5 elements
-            for (var createElement=0; createElement < 4; createElement++) {
-                var element = $('#element').first().clone();
-                $(element).appendTo('.dashBody ul');
+            // Little functions
+            let makeDisabledAble = (disabled) => {
+                if ($(disabled).prop('disabled')) {
+                    $(disabled).prop('disabled', false);
+                }
             }
 
+            let createFive = (parent) => {
+                for (var createElement=0; createElement < 4; createElement++) {
+                    var element = $('#element').first().clone();
+                    $(element).appendTo('.dashBody ul').eq(parent);
+                }
+            }
+
+            // For the first dashboard
+            createFive(0);
+
+            // Button functions in dash
             $('#addNewElement').click(function(event){
                 event.preventDefault();
 
                 var parentDash = $(this).parent();
                 var parentIndex = parentDash.index();
+                var newElementButton = $(parentDash).eq(parentIndex-1).find(this);
+                var removeElement = $(parentDash).eq(parentIndex-1).find('#removeElement');
 
                 if (dashElementHandler[parentIndex-1] === 10) {
-                    var newElementButton = $(parentDash).eq(parentIndex-1).find(this);
                     $(newElementButton).prop('disabled', true);
 
                 } else {
                     var cloneElement = $('.dashBody ul').eq(parentIndex-1).find('#element').first().clone();
                     $(cloneElement).appendTo('.dashBody ul').eq(parentIndex-1);
-
                     dashElementHandler[parentIndex-1]++;
+                    makeDisabledAble(removeElement);
                 }
+            });
+
+            $('#removeElement').click(function(event){
+                event.preventDefault();
+
+                var parentDash = $(this).parent();
+                var parentIndex = parentDash.index();
+                var removeElementBTN = $(parentDash).eq(parentIndex-1).find(this);
+                var addElement = $(parentDash).eq(parentIndex-1).find('#addNewElement');
+
+                if (dashElementHandler[parentIndex-1] === 1) {
+                    $(removeElementBTN).prop('disabled', true);
+
+                } else {
+                    var elementToRem = $('.dashBody ul').eq(parentIndex-1).find('#element').last().remove();
+                    dashElementHandler[parentIndex-1]--;
+                    makeDisabledAble(addElement);
+                }
+            });
+
+            // Button functions out dash
+            $('#createNewDash').click(function(event){
+                event.preventDefault();
+
+                $('.dashboard').first().clone().appendTo('.dashes');
+                
+                createFive($(newDash));
             });
         });
 
