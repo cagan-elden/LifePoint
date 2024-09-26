a688789386c4a50c50dea9cca294d96b41949c87
Cagan Eldenk <caganeldenk@gmail.com>
2024-09-26 21:30:20 +0300

    Turned today dashboard into a function so it would save time and space from writing another copy for the profile.

--- app/dashboard/dashboard/today.php
+++ app/dashboard/dashboard/today.php
@@ -2,78 +2,93 @@
 
 include '../databaseConn.php';
 
-$date = new DateTime();
-$date = $date->format('d.m.Y');
+function todayType($type) {
+    include '../databaseConn.php';
 
-$query = 'SELECT * FROM chore WHERE userId=:userId AND date=:date';
-                
-$getDay = $conn->prepare($query);
-$getDay->bindParam(':userId', $_SESSION['userId'], PDO::PARAM_STR);
-$getDay->bindParam(':date', $date, PDO::PARAM_STR);
-$getDay->execute();
+    if ($type == 'dashboard') { $user = $_SESSION['userId']; } else if($type == 'profile') { $user = $user; }
 
-$choreExist = $getDay->rowCount();
+    $date = new DateTime();
+    $date = $date->format('d.m.Y');
 
-?>
+    $query = 'SELECT * FROM chore WHERE userId=:userId AND date=:date';
+                        
+    $getDay = $conn->prepare($query);
+    $getDay->bindParam(':userId', $user, PDO::PARAM_STR);
+    $getDay->bindParam(':date', $date, PDO::PARAM_STR);
+    $getDay->execute();
 
-<div class="dashboard">
-    <div class="dashHeader">
-        <h2 id="dashTitle">Today</h2>
-        <?php
-        
-        if ($choreExist) {
+    $choreExist = $getDay->rowCount();
+
+    ?>
+
+    <div class="dashboard">
+        <div class="dashHeader">
+            <h2 id="dashTitle">Today</h2>
+            <?php
+            
+            if ($choreExist) {
+                ?>
+                    <a href="complete.php" id="dashHref">
+                        <img src="../source/image/mainApp/check.svg" id="dashIcon">
+                    </a>  
+                <?php
+            } else {
+                ?>
+                    <a href="plan.php" id="dashHref">
+                        <img src="../source/image/mainApp/editBlack.svg" id="dashIcon">
+                    </a>
+                <?php
+            }
+            
             ?>
-                <a href="complete.php" id="dashHref">
-                    <img src="../source/image/mainApp/check.svg" id="dashIcon">
-                </a>  
-            <?php
-        } else {
-            ?>
-                <a href="plan.php" id="dashHref">
-                    <img src="../source/image/mainApp/editBlack.svg" id="dashIcon">
-                </a>
-            <?php
-        }
-        
-        ?>
-    </div>
-    <div class="dashBody" <?php echo !$choreExist ? 'id="dashAlignItemsCenter"' : ''; ?>>
-            <?php
-                if ($choreExist) {
-                    $chores = $getDay->fetchAll(PDO::FETCH_ASSOC);
+        </div>
+        <div class="dashBody" <?php echo !$choreExist ? 'id="dashAlignItemsCenter"' : ''; ?>>
+                <?php
+                    if ($choreExist) {
+                        $chores = $getDay->fetchAll(PDO::FETCH_ASSOC);
 
-                    ?>
-                    <ul>
-                    <?php
-                        foreach ($chores as $chore) {
-                            ?>
-                            <li id="dashItem">
-                                <?php
-
-                                    if ($chore['status'] == "done") {
-                                        ?>
-                                        <span id="time" style="background-color: lightgreen;"><?php echo $chore['time']; ?></span>
-                                        <?php 
-                                    } else { 
-                                        ?>
-                                        <span id="time"><?php echo $chore['time']; ?></span>
-                                        <?php
-                                    }
-                                    
-                                    echo $chore['chore'];
-
+                        ?>
+                        <ul>
+                        <?php
+                            foreach ($chores as $chore) {
                                 ?>
-                            </li>
-                            <?php
-                        }
-                    ?>
-                    </ul>
-                    <?php
-                } else {
-                    ?>
-                    <span id="detail">You don't have any plan for today, <a href="plan.php" id="spanHref">plan today</a>.</span>
-                    <?php
-                }
-            ?>
+                                <li id="dashItem">
+                                    <?php
+
+                                        if ($chore['status'] == "done") {
+                                            ?>
+                                            <span id="time" style="background-color: lightgreen;"><?php echo $chore['time']; ?></span>
+                                            <?php 
+                                        } else { 
+                                            ?>
+                                            <span id="time"><?php echo $chore['time']; ?></span>
+                                            <?php
+                                        }
+                                        
+                                        echo $chore['chore'];
+
+                                    ?>
+                                </li>
+                                <?php
+                            }
+                        ?>
+                        </ul>
+                        <?php
+                    } else if ($user = $_SESSION['userId']){
+                        ?>
+                        <span id="detail">You don't have any plan for today, <a href="plan.php" id="spanHref">plan today</a>.</span>
+                        <?php
+                    } else {
+                        ?>
+                        <span id="detail">Looks like user doesn't have any plan.</span>   
+                        <?php
+                    }
+                ?>
+        </div>
     </div>
-</div>
+
+<?php
+}
+
+todayType('dashboard');
+?>
