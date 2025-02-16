9d0f228eb9bf38c44ed4fd910bee0debf078e87e
Cagan Eldenk <caganeldenk@gmail.com>
2025-02-16 18:49:01 +0300

    Worked with ajax & jquery not finished

--- app/script/php/checkFriendReq.php
+++ app/script/php/checkFriendReq.php
@@ -13,19 +13,17 @@ if ($numReq > 0) {
     $reqOrder = array(); // Stores the requests by request id and decides which to show
 
     foreach ($getReq as $req) {
-        $query = 'SELECT profilePic, userId, username FROM user WHERE userId=:id';
-        $prepareDataQuery = $conn->prepare($query);
-        $prepareDataQuery->bindParam(':id', $req['notificationFrom']);
-        $prepareDataQuery->execute();
-        $userData = $prepareDataQuery->fetch(PDO::FETCH_ASSOC);
-
         array_push($reqOrder, $req['notificationId']);
+    }
+}
 
-        if (array_search($req['notificationId'], $reqOrder) == 0) {
-            ?>
+?>
+
+<!--
+
             <div class="friendNotify">
-                <img src="<?php echo $userData['profilePic']; ?>" id="profilePicture">
-                <span id="notificationNum"><?php echo $numReq; ?></span>
+                <img src="" id="profilePicture">
+                <span id="notificationNum"></span>
             </div>
 
             <div class="friendDetail">
@@ -38,9 +36,5 @@ if ($numReq > 0) {
                 <button id="acceptReq">Accept</button>
                 <button id="rejectReq">Reject</button>
             </div>
-            <?php
-        }
-    }
-}
 
-?>
+-->
--- source/frontend/jquery/getUserDetFriendReq.js
+++ source/frontend/jquery/getUserDetFriendReq.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
+    const pageUrl = window.location.href;
+
     $('.friendNotify').click(function() {
         $('.friendDetail').toggle(); 
+    });
+
+    $('#acceptReq').click(function(){
+        $.ajax({
+                url: pageUrl,
+                method: 'POST',
+                success: function (response) {
+                }
+        });
     });
 });
